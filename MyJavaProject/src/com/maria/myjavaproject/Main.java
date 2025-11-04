4d6b132acc298c161bdc951473d131af5206126d
Voichita Maria <sabo_voichita@yahoo.com>
2025-11-04 10:43:52 +0100

    The Print() Method

--- MyJavaProject/src/com/maria/myjavaproject/Main.java
+++ MyJavaProject/src/com/maria/myjavaproject/Main.java
@@ -13,10 +13,17 @@ package com.maria.myjavaproject;
 //	}
 //}
 
+//public class Main{
+//	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//		System.out.println("I am learning Java");
+//		System.out.println("It is awesome!");
+//	}
+//}
+
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("I am learning Java");
-		System.out.println("It is awesome!");
+		System.out.print("Hello World! ");
+		System.out.print("I will print on the same line.");
 	}
 }
