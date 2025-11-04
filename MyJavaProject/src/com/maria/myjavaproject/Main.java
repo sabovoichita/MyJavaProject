970cd9546d46f549266e212c9b0ed5f35d7efa82
Voichita Maria <sabo_voichita@yahoo.com>
2025-11-04 10:36:47 +0100

    Add more lines of code to print

--- MyJavaProject/src/com/maria/myjavaproject/Main.java
+++ MyJavaProject/src/com/maria/myjavaproject/Main.java
@@ -6,9 +6,17 @@ package com.maria.myjavaproject;
 //    }
 //}
 
-public class Main {
-	public static void main(String[]args) {
-		
-		System.out.println("Hello");
+//public class Main {
+//	public static void main(String[]args) {
+//		
+//		System.out.println("Hello");
+//	}
+//}
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("HAVE A GOOD DAY!");
+		System.out.println("Learning Java is fun!");
 	}
 }
