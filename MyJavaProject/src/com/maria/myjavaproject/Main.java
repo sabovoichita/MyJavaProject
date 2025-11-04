d219ba05e8bfb51d536dd773c84d7f7fc8c9d8e8
Voichita Maria <sabo_voichita@yahoo.com>
2025-11-04 10:09:33 +0100

    comment out last code

--- MyJavaProject/src/com/maria/myjavaproject/Main.java
+++ MyJavaProject/src/com/maria/myjavaproject/Main.java
@@ -1,14 +1,14 @@
 package com.maria.myjavaproject;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
 
 public class Main {
 	public static void main(String[]args) {
 		String name = "Maria";
-		System.out.println("Hello"+ name);
+		System.out.println("Hello "+ name);
 	}
 }
