16822ae56f60b3f3233eedf9fbdc8b0896791591
Voichita Maria <sabo_voichita@yahoo.com>
2025-11-04 10:19:52 +0100

    Hello

--- MyJavaProject/src/com/maria/myjavaproject/Main.java
+++ MyJavaProject/src/com/maria/myjavaproject/Main.java
@@ -8,7 +8,7 @@ package com.maria.myjavaproject;
 
 public class Main {
 	public static void main(String[]args) {
-		String name = "Maria";
-		System.out.println("Hello "+ name);
+		
+		System.out.println("Hello");
 	}
 }
