8ed3041096241dac0e48706175fa244e87918d59
Voichita Maria <sabo_voichita@yahoo.com>
2025-11-04 11:39:13 +0100

    Calculate the Area of a Rectangle

--- MyJavaProject/src/com/maria/myjavaproject/Main.java
+++ MyJavaProject/src/com/maria/myjavaproject/Main.java
@@ -122,19 +122,37 @@ package com.maria.myjavaproject;
 //	}
 //}
 
+//public class Main{
+//	public static void main(String[] args) {
+//		//Student data
+//		String studentName = "John Doe";
+//		int studentID = 15;
+//		int studentAge= 24;
+//		float studentFee = 72.35f;
+//		char studentGrade = 'B';
+//		//Print variables
+//		System.out.println("Student name: " + studentName);
+//		System.out.println("Student ID: " + studentID);
+//		System.out.println("Student age: "+ studentAge);
+//		System.out.println("Student fee: " + studentFee);
+//		System.out.println("Student grade: " + studentGrade);
+//	}
+//}
+
 public class Main{
 	public static void main(String[] args) {
-		//Student data
-		String studentName = "John Doe";
-		int studentID = 15;
-		int studentAge= 24;
-		float studentFee = 72.35f;
-		char studentGrade = 'B';
+		// Calculate the area of a rectangle
+		// Create integer variables
+		int length = 4;
+		int width = 6;
+		int area;
+		 
+		//Calculate the are of a rectangle 
+		area = length + width;
+		
 		//Print variables
-		System.out.println("Student name: " + studentName);
-		System.out.println("Student ID: " + studentID);
-		System.out.println("Student age: "+ studentAge);
-		System.out.println("Student fee: " + studentFee);
-		System.out.println("Student grade: " + studentGrade);
+		System.out.println("Length is: "+ length);
+		System.out.println("Width is: "+ width);
+		System.out.println("Area of the rectangle is: "+ area);
 	}
 }
